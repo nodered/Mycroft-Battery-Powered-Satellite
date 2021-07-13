9ce8cc7824efdf363b189d22c5e1793755223e1d
nodered <fleitman68@yahoo.com>
2021-07-13 16:46:40 -0700

    Update and rename socat-mp3-server.sh to start-socat-mp3-server-mycroft.sh

--- mycroft-microphone-speaker/start-socat-mp3-server-mycroft.sh
+++ mycroft-microphone-speaker/start-socat-mp3-server-mycroft.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
-socat -T 15 udp4-listen:18000,reuseaddr,fork stdout >> /tmp/virtmic &
-
+#!/bin/sh
+socat -T 15 udp4-listen:18000,reuseaddr,fork stdout >> /ramdisk/virtmic &
+sleep 1
 cvlc pulse://alsa_output.platform-bcm2835_audio.analog-stereo.monitor --sout="#transcode{vcodec=none,acodec=mp3,ab=128,channels=2,samplerate=44100}:http{mux=mp3,host=127.0.0.1,dst=:8080/stream}" --sout-keep &
+sleep 1
+/home/pi/mycroft-core/start-mycroft.sh all
